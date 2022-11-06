fd2817b4f1ae6658b1738e40512509f0ef730814
Shahzaib <51841651+m3zai6i@users.noreply.github.com>
2022-11-06 19:14:25 +0500

    init

---
+++ train.sh
@@ -0,0 +1,4 @@
+python train.py --img 416 --batch 16 --epochs 30 --data ./coco.yaml --cfg yolov5s.yaml --weights yolov5s.pt --name model_n1 --device cpu
+# python train.py --img 416 --batch 16 --epochs 30 --data ./coco.yaml --cfg yolov5m.yaml --weights yolov5m.pt --name model_n1 --device cpu
+# python train.py --img 416 --batch 16 --epochs 30 --data ./coco.yaml --cfg yolov5l.yaml --weights yolov5l.pt --name model_n1 --device cpu
+# python train.py --img 416 --batch 16 --epochs 30 --data ./coco.yaml --cfg yolov5x.yaml --weights yolov5x.pt --name model_n1 --device cpu
